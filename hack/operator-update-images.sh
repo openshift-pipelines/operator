c52343f373ec5536c792fa19d54ec0b795ef28a5
Vincent Demeester <vdemeest@redhat.com>
2024-10-08 15:09:47 +0200

    hack/image-mapping: update images and handle operator's images
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- hack/operator-update-images.sh
+++ hack/operator-update-images.sh
@@ -12,3 +12,9 @@ for i in $(seq 0 $((${LENGTH}-1))); do
     yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].env[] | select(.name == \"${NAME}\") | .value) = \"${REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
     yq eval --inplace "(.spec.relatedImages[] | select(.name == \"${NAME}\") | .image) = \"${REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
 done
+
+# Operator's specifics
+OPERATOR_REFERENCE=$(yq e '.images[] | select(.name == "OPENSHIFT_PIPELINES_OPERATOR_LIFECYCLE") | .value' image-mapping.yaml)
+WEBHOOK_REFERENCE=$(yq e '.images[] | select(.name == "TEKTON_OPERATOR_WEBHOOK") | .value' image-mapping.yaml)
+yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].image) = \"${OPERATOR_REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
+yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-webhook\")| .spec.template.spec.containers[].image) = \"${WEBHOOK_REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
