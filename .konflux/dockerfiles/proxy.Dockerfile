e8e13a7f297336a2a5acaa5fc8571d4e4129f970
Vincent Demeester <vdemeest@redhat.com>
2024-11-05 11:06:47 +0100

    Konflux: move from openshift to .konflux…
    … as this is the convention (enforce by hack as well)
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- .konflux/dockerfiles/proxy.Dockerfile
+++ .konflux/dockerfiles/proxy.Dockerfile
@@ -1,16 +1,16 @@
-ARG GO_BUILDER=brew.registry.redhat.io/rh-osbs/openshift-golang-builder:v1.22
-ARG RUNTIME=registry.access.redhat.com/ubi9/ubi-minimal:latest@sha256:c0e70387664f30cd9cf2795b547e4a9a51002c44a4a86aa9335ab030134bf392
+arg go_builder=brew.registry.redhat.io/rh-osbs/openshift-golang-builder:v1.22
+arg runtime=registry.access.redhat.com/ubi9/ubi-minimal:latest@sha256:c0e70387664f30cd9cf2795b547e4a9a51002c44a4a86aa9335ab030134bf392
 
-FROM $GO_BUILDER AS builder
+from $go_builder as builder
 
-WORKDIR /go/src/github.com/tektoncd/operator
-COPY upstream .
-# FIXME: handle patches (maybe ? probably not needed though)
-# COPY patches patches/
-# RUN set -e; for f in patches/*.patch; do echo ${f}; [[ -f ${f} ]] || continue; git apply ${f}; done
-# ENV CHANGESET_REV=$CI_OPERATOR_UPSTREAM_COMMIT
-ENV GODEBUG="http2server=0"
-RUN go build -ldflags="-X 'knative.dev/pkg/changeset.rev=${CHANGESET_REV:0:7}'" -mod=vendor -o /tmp/openshift-pipelines-operator-proxy \
+workdir /go/src/github.com/tektoncd/operator
+copy upstream .
+# fixme: handle patches (maybe ? probably not needed though)
+# copy patches patches/
+# run set -e; for f in patches/*.patch; do echo ${f}; [[ -f ${f} ]] || continue; git apply ${f}; done
+# env changeset_rev=$ci_operator_upstream_commit
+env godebug="http2server=0"
+run go build -ldflags="-X 'knative.dev/pkg/changeset.rev=${changeset_rev:0:7}'" -mod=vendor -o /tmp/openshift-pipelines-operator-proxy \
     ./cmd/openshift/proxy-webhook
 # RUN /bin/sh -c 'echo $CI_PIPELINE_UPSTREAM_COMMIT > /tmp/HEAD'
 
--- hack/operator-update-images.sh
+++ hack/operator-update-images.sh
@@ -9,12 +9,12 @@ for i in $(seq 0 $((${LENGTH}-1))); do
 
     echo "${NAME}: ${REFERENCE}"
 
-    yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].env[] | select(.name == \"${NAME}\") | .value) = \"${REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
-    yq eval --inplace "(.spec.relatedImages[] | select(.name == \"${NAME}\") | .image) = \"${REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
+    yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].env[] | select(.name == \"${NAME}\") | .value) = \"${REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
+    yq eval --inplace "(.spec.relatedImages[] | select(.name == \"${NAME}\") | .image) = \"${REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
 done
 
 # Operator's specifics
 OPERATOR_REFERENCE=$(yq e '.images[] | select(.name == "OPENSHIFT_PIPELINES_OPERATOR_LIFECYCLE") | .value' project.yaml)
 WEBHOOK_REFERENCE=$(yq e '.images[] | select(.name == "TEKTON_OPERATOR_WEBHOOK") | .value' project.yaml)
-yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].image) = \"${OPERATOR_REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
-yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"tekton-operator-webhook\")| .spec.template.spec.containers[].image) = \"${WEBHOOK_REFERENCE}\"" openshift/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
+yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].image) = \"${OPERATOR_REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
+yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"tekton-operator-webhook\")| .spec.template.spec.containers[].image) = \"${WEBHOOK_REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
