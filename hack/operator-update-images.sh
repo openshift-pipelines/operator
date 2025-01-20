869d6b83833dc90c520266c7cb5a588e31901d8d
Pramod Bindal <bindal.pramod@gmail.com>
2025-01-20 12:07:48 +0530

    use images from openshift-pipeline namespace for release versions

--- hack/operator-update-images.sh
+++ hack/operator-update-images.sh
@@ -3,10 +3,16 @@
 
 LENGTH=$(yq e '.images | length' project.yaml)
 
+KONFLUX_TENANT_IMAGE_REPO='redhat-user-workloads\/tekton-ecosystem-tenant'
+RELEASE_IMAGE_REPO=openshift-pipeline
+
 for i in $(seq 0 $((${LENGTH}-1))); do
     NAME=$(yq e ".images.${i}.name" project.yaml)
-    REFERENCE=$(yq e ".images.${i}.value" project.yaml)    
+    REFERENCE=$(yq e ".images.${i}.value" project.yaml)
 
+    if [[ $REFERENCE == *${KONFLUX_TENANT_IMAGE_REPO}* ]]; then
+       REFERENCE=$(echo $REFERENCE | sed -e "s/$KONFLUX_TENANT_IMAGE_REPO/$RELEASE_IMAGE_REPO/" -e 's/\(.*\)\//\1-/')
+    fi
     echo "${NAME}: ${REFERENCE}"
 
     yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].env[] | select(.name == \"${NAME}\") | .value) = \"${REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
