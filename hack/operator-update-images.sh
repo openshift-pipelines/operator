d7c6be5a2487a704352c2474cf5c6d5a094d9f4c
savitaashture <sashture@redhat.com>
2025-02-07 18:13:02 +0530

    update operator-update-images.sh to avoid replacing the images
    Signed-off-by: savitaashture <sashture@redhat.com>

--- hack/operator-update-images.sh
+++ hack/operator-update-images.sh
@@ -3,19 +3,11 @@
 
 LENGTH=$(yq e '.images | length' project.yaml)
 
-KONFLUX_TENANT_IMAGE_REPO='redhat-user-workloads\/tekton-ecosystem-tenant'
-RELEASE_IMAGE_REPO=openshift-pipeline
-
 for i in $(seq 0 $((${LENGTH}-1))); do
     NAME=$(yq e ".images.${i}.name" project.yaml)
-    REFERENCE=$(yq e ".images.${i}.value" project.yaml)
+    REFERENCE=$(yq e ".images.${i}.value" project.yaml)    
 
-    # Until operator release plan is completed. Lets use the IMAGE_PIPELINES_PROXY from redhat-user-workloads/tekton-ecosystem-tenant
-    if [[ $REFERENCE == *${KONFLUX_TENANT_IMAGE_REPO}* && $NAME != "IMAGE_PIPELINES_PROXY" ]]; then
-       REFERENCE=$(echo $REFERENCE | sed -e "s/$KONFLUX_TENANT_IMAGE_REPO/$RELEASE_IMAGE_REPO/" -e 's/\(.*\)\//\1-/')
-       echo "${NAME}: ${REFERENCE}"
-    fi
-
+    echo "${NAME}: ${REFERENCE}"
 
     yq eval --inplace "(.spec.install.spec.deployments[] | select(.name == \"openshift-pipelines-operator\")| .spec.template.spec.containers[].env[] | select(.name == \"${NAME}\") | .value) = \"${REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
     yq eval --inplace "(.spec.relatedImages[] | select(.name == \"${NAME}\") | .image) = \"${REFERENCE}\"" .konflux/olm-catalog/bundle/manifests/openshift-pipelines-operator-rh.clusterserviceversion.yaml
