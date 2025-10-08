544ef5538623eca06b364f10f5cbdc197ade47ad
Pramod Bindal <prbindal@redhat.com>
2025-10-08 14:29:49 +0530

    update-version.sh increases versions for nightly builds only

--- hack/update-version.sh
+++ hack/update-version.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
-
-set -ex
+set -e
 current_version=$(yq e '.versions.current' project.yaml)
-echo $current_version
-current_version_number=${current_version##*-}
-echo $current_version_number
-new_version_number=$(( current_version_number + 1 ))
-echo $new_version_number
-new_version1=(${current_version//-/ })
-new_version2=$new_version1-$new_version_number
-echo "$new_version2"
-yq -i e ".versions.current = \"$new_version2\"" project.yaml
-yq -i e ".versions.previous = \"$current_version\"" project.yaml
+echo "Current Version : $current_version"
+new_version2=$current_version
+
+# If version followed by build number then  update the current version too
+if [[ $current_version == *-* ]]; then
+  current_version_number=${current_version##*-}
+  echo "current_version_number: $current_version_number"
+
+  new_version_number=$(( current_version_number + 1 ))
+  echo "new_version_number:  $new_version_number"
+
+  new_version1=(${current_version//-/ })
+  new_version2=$new_version1-$new_version_number
+  echo "new_version: $new_version2"
+
+  yq -i e ".versions.current = \"$new_version2\"" project.yaml
+  yq -i e ".versions.previous = \"$current_version\"" project.yaml
+fi
 
 # update version and previous_version in operator-fetch-payload
 sed -i "s%version=.*\\\%version=\"$new_version2\" \\\%g" .konflux/olm-catalog/bundle/Dockerfile
