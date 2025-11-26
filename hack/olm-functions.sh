ac34994bb28afc721c966c731f214470f97b03d5
Pramod Bindal <prbindal@redhat.com>
2025-11-26 21:42:28 +0530

    Generate All catalogs with single command

--- hack/olm-functions.sh
+++ hack/olm-functions.sh
@@ -1,17 +1,28 @@
-  function get_bundle_image() {
+  function update_bundle_image() {
     environment=${1:-"devel"}
+    BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    ROOT_DIR="$(dirname "$BASEDIR")"
+
     echo "Getting bundle image for environment: $environment" >&2
     TARGET_REGISTRY=$(target_registry $environment)
     echo "Target registry: $TARGET_REGISTRY" >&2
     BUNDLE_IMAGE=$(yq '.images[] | select(.name == "IMAGE_OPERATOR_BUNDLE") | .value' project.yaml)
     echo "Original bundle image: $BUNDLE_IMAGE" >&2
     BUNDLE_JSON=$(opm render --skip-tls-verify -o json ${BUNDLE_IMAGE})
+    BUNDLE_NAME=$(echo $BUNDLE_JSON | jq -r '.name')
+    BUNDLE_VERSION=$(echo $BUNDLE_NAME | awk -F 'v' '{ print $2 }')
+    echo "BUNDLE_NAME : $BUNDLE_NAME"
+    echo "Bundle Version : $BUNDLE_VERSION"
 
     SOURCE_PATTEN="quay.io/.*/(.*)-rhel9(@sha256:.+)"
     TARGET_PATTEN="$TARGET_REGISTRY/pipelines-\1\2"
     BUNDLE_IMAGE=$(echo "${BUNDLE_IMAGE}" | sed -E "s|$SOURCE_PATTEN|$TARGET_PATTEN|g")
     echo "Updated bundle image: $BUNDLE_IMAGE" >&2
-    echo $BUNDLE_IMAGE
+
+    #Update the bundle image and version in the olm config.yaml file only for 5.0.x versions
+    export BUNDLE_IMAGE BUNDLE_VERSION
+    yq -i e '(.bundles[] | select(.version | test("^5\\.0\\..*"))).image = env(BUNDLE_IMAGE)' $ROOT_DIR/olm/config.yaml
+    yq -i e '(.bundles[] | select(.version | test("^5\\.0\\..*"))).version = env(BUNDLE_VERSION)' $ROOT_DIR/olm/config.yaml
 }
 
 function target_registry() {
---
+++ hack/update-olm-bundle.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+ENVIRONMENT=${1:-"devel"}
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(dirname "$BASEDIR")"
+
+source $BASEDIR/olm-functions.sh
+
+update_bundle_image $ENVIRONMENT
