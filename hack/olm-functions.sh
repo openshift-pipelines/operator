7204ba27777da1a039a17cd411fdc91338b2acde
Pramod Bindal <prbindal@redhat.com>
2026-04-17 13:27:16 +0530

    Add Mirror and policy in GHA

--- hack/olm-functions.sh
+++ hack/olm-functions.sh
@@ -66,10 +66,10 @@ function render_catalog() {
     RENDERED_CATALOG_JSON=$3
     NUMERIC_VERSION=${VERSION#v} # Removes "v" prefix
 
-    if (( $(echo "$NUMERIC_VERSION >= 4.17" | bc -l) )); then
-        opm alpha render-template basic $CATALOG_JSON --migrate-level=bundle-object-to-csv-metadata > $RENDERED_CATALOG_JSON
+    if [[ "$(printf "%s\n%s\n" "${NUMERIC_VERSION#v}" "4.17" | sort -V | head -n1)" == "4.17" ]]; then
+      opm alpha render-template basic "$CATALOG_JSON" --migrate-level=bundle-object-to-csv-metadata > "$RENDERED_CATALOG_JSON"
     else
-      opm alpha render-template basic $CATALOG_JSON > $RENDERED_CATALOG_JSON
+      opm alpha render-template basic "$CATALOG_JSON" > "$RENDERED_CATALOG_JSON"
     fi
     echo "Render template for $VERSION Done"
 
