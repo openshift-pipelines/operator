5c4b07d53b29ab1779ac6313a81ad6dd633ddb66
Pramod Bindal <prbindal@redhat.com>
2026-07-07 10:58:40 +0530

    update bundle should update the last version instead of adding new version

--- hack/olm-functions.sh
+++ hack/olm-functions.sh
@@ -68,7 +68,7 @@ function render_catalog() {
     RENDERED_CATALOG_JSON=$3
     NUMERIC_VERSION=${VERSION#v} # Removes "v" prefix
 
-    if (( $(echo "$NUMERIC_VERSION >= 4.17" | bc -l) )); then
+    if (( $(awk -v ver="$NUMERIC_VERSION" 'BEGIN { print (ver >= 4.17) }') )); then
         opm alpha render-template basic $CATALOG_JSON --migrate-level=bundle-object-to-csv-metadata > $RENDERED_CATALOG_JSON
     else
       opm alpha render-template basic $CATALOG_JSON > $RENDERED_CATALOG_JSON
